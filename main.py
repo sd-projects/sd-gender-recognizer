0c26eb7d40348c7c27dc5dc3e0782482d2b640e8
Sagiman <sasha@sagiman.ru>
2023-04-14 23:44:38 +0700

    some changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from layouts.layout_main import Ui_MainRecognizerWindow
 
 # Потом нужно заменить на натоящую функцию
-from time import sleep
 from archive.s_gui.test_alg import gender
 
 from PySide6.QtWidgets import QMainWindow, QFileDialog, QApplication, QMessageBox
