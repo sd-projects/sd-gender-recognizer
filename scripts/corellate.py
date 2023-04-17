38c47975f84071f596288db0b988abd0e8c2ea5e
aminealist <aleshkov.d.o@gmail.com>
2023-04-17 09:05:30 +0700

    17_04_2023

--- scripts/corellate.py
+++ scripts/corellate.py
@@ -7,69 +7,69 @@ from scipy.io import wavfile
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 inpt = os.listdir("D:\github\sd-gender-recognizer\d_backend\Material for tests\others")
 audio_dt_name = inpt
 st = ""
 k = 0
 for i in range(22):
     if k < 15:
-        colr="--g"
+        colr = "--g"
     else:
-        colr=":m"
-    k+=1
-    st+=f"[i for i in range(len(mfccsdf[{i}]))], mfccsdf[{i}], '{colr}', "
+        colr = ":m"
+    k += 1
+    st += f"[i for i in range(len(mfccsdf[{i}]))], mfccsdf[{i}], '{colr}', "
 print(st)
 df = []
 mfccsdf = []
 for i, audio_data in enumerate(audio_dt_name):
-    a=0
-    if audio_data[:3]=="man":
-        a=1
+    a = 0
+    if audio_data[:3] == "man":
+        a = 1
     y, sr = librosa.load(audio_data)
     y_harmonic, y_percussive = librosa.effects.hpss(y)
-    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,sr=sr)
+    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
     mfccs = librosa.feature.mfcc(y=y_harmonic, sr=sr, n_mfcc=13)
     X = librosa.stft(y)
     Xdb = librosa.amplitude_to_db(abs(X))
     cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-    contrast=librosa.feature.spectral_contrast(y=y_harmonic,sr=sr)
+    contrast = librosa.feature.spectral_contrast(y=y_harmonic, sr=sr)
     rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
     zero_crossing = []
     shag = 500
-    zero_count = len(y)//shag
+    zero_count = len(y) // shag
     counter = 0
     print(audio_data)
     sample_rate, audio = wavfile.read(audio_data)
-    tm =(len(audio) / sample_rate)
+    tm = (len(audio) / sample_rate)
     print(np.mean(mfccsdf))
     print(tm)
     for i in range(zero_count):
-        zero_crossing.append(sum(librosa.zero_crossings(y[counter:counter+shag], pad=False)))
-        counter+=shag
+        zero_crossing.append(sum(librosa.zero_crossings(y[counter:counter + shag], pad=False)))
+        counter += shag
     mfccsdf.append([np.max(mfccs[0]), max(mfccs[1]), max([2]), max(mfccs[3]), max(mfccs[4]), max(mfccs[5]),
                     max(mfccs[6]), max(mfccs[7]), max(mfccs[8]), max(mfccs[9]), max(mfccs[10]),
                     max(mfccs[11]), max(mfccs[12])])
 
-    df.append([a, audio_data, tm , np.max(rolloff), np.mean(mfccsdf), np.sum(y_harmonic)/tm, np.sum(y_percussive)/tm,
-               np.mean(y_harmonic)*10**10, np.mean(y_percussive)*10**10, np.var(y_harmonic)*10**4
-               , np.var(y_percussive)*10**4, np.median(y_harmonic)*10**10, np.median(y_percussive)*10**10,
-               np.sum(mfccs)/tm, np.mean(mfccs), np.var(mfccs)
-               , np.sum(Xdb)/tm, np.mean(Xdb), np.var(X), np.var(Xdb), np.var(zero_crossing), np.mean(zero_crossing),
-               np.sum(zero_crossing)/tm, np.median(zero_crossing)])
+    df.append([a, audio_data, tm, np.max(rolloff), np.mean(mfccsdf), np.sum(y_harmonic) / tm, np.sum(y_percussive) / tm,
+               np.mean(y_harmonic) * 10 ** 10, np.mean(y_percussive) * 10 ** 10, np.var(y_harmonic) * 10 ** 4
+                  , np.var(y_percussive) * 10 ** 4, np.median(y_harmonic) * 10 ** 10,
+               np.median(y_percussive) * 10 ** 10,
+               np.sum(mfccs) / tm, np.mean(mfccs), np.var(mfccs)
+                  , np.sum(Xdb) / tm, np.mean(Xdb), np.var(X), np.var(Xdb), np.var(zero_crossing),
+               np.mean(zero_crossing),
+               np.sum(zero_crossing) / tm, np.median(zero_crossing)])
 print(df)
 
-
-df = pd.DataFrame(df, columns=[" sex", " audio_data", "time", "np.mean(contrast)","np.mean(mfccsdf)", "np.sum(y_harmonic)",
-                               " np.sum(y_percussive)", " np.mean(y_harmonic)*10**10", " np.mean(y_percussive)*10**10",
-                               " np.var(y_harmonic)*10**4", "np.var(y_percussive)*10**4",
-                               " np.median(y_harmonic)*10**10", " np.median(y_percussive)*10**10",
-                               " np.sum(mfccs)", " np.mean(mfccs)", " np.var(mfccs)", " np.sum(Xdb)",
-                               " np.mean(Xdb)", " np.var(X)", "np.var(Xdb)", "np.var(zero_crossing)",
-                               "np.mean(zero_crossing)", "np.sum(zero_crossing)", "np.median(zero_crossing)"])
+df = pd.DataFrame(df,
+                  columns=[" sex", " audio_data", "time", "np.mean(contrast)", "np.mean(mfccsdf)", "np.sum(y_harmonic)",
+                           " np.sum(y_percussive)", " np.mean(y_harmonic)*10**10", " np.mean(y_percussive)*10**10",
+                           " np.var(y_harmonic)*10**4", "np.var(y_percussive)*10**4",
+                           " np.median(y_harmonic)*10**10", " np.median(y_percussive)*10**10",
+                           " np.sum(mfccs)", " np.mean(mfccs)", " np.var(mfccs)", " np.sum(Xdb)",
+                           " np.mean(Xdb)", " np.var(X)", "np.var(Xdb)", "np.var(zero_crossing)",
+                           "np.mean(zero_crossing)", "np.sum(zero_crossing)", "np.median(zero_crossing)"])
 
 df.to_excel("data.xlsx", index=False)
-
 
 df = pd.read_excel("data.xlsx")
 df.dropna()
@@ -88,13 +88,10 @@ df1 = df
 #          ':m', [i for i in range(len(mfccsdf[20]))], mfccsdf[20], ':m',[i for i in range(len(mfccsdf[21]))], mfccsdf[21], ':m')
 
 
-
-plt.figure(figsize=(12,10), dpi= 80)
-sns.heatmap(df1.corr(), xticklabels=df1.corr().columns, yticklabels=df1.corr().columns, cmap='RdYlGn', center=0, annot=True)
+plt.figure(figsize=(12, 10), dpi=80)
+sns.heatmap(df1.corr(), xticklabels=df1.corr().columns, yticklabels=df1.corr().columns, cmap='RdYlGn', center=0,
+            annot=True)
 plt.title('Correlogram', fontsize=22)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.show()
-
-
-
--- scripts/wav_convert_mp3_remove.py
+++ scripts/wav_convert_mp3_remove.py
@@ -1,8 +1,6 @@
 import os
 from os import path
-import librosa
 from pydub import AudioSegment
-import numpy as np
 
 
 # Для использования этой функции нужна программа "ffmpeg.exe"
@@ -27,45 +25,3 @@ def all_convert():
     # Перебор всех файлов из папки
     for i in [i for i in os.listdir() if i[-4:] == ".mp3" or i[-4:] == "wav"]:
         wav_convert(i)
-
-
-def s_sr(lstdir):
-    for i in lstdir:
-        y, sr = librosa.load(i)
-        print(Xdb_criterion(y=y), i)
-        # print(y_percussive_criterion(y)[1], mfccs_criterion(y, sr)[1], mfccs_criterion(y, sr)[2], mfccs_criterion(y, sr)[3], i)
-
-
-def mfccs_criterion(y, sr):
-    y_harmonic = librosa.effects.hpss(y)[0]
-    mfccs = librosa.feature.mfcc(y=y_harmonic, sr=sr, n_mfcc=13)
-    result1 = np.mean([max(i) for i in mfccs])
-    result2 = max(mfccs[1])
-    result3 = max(mfccs[2])
-    if result1 > 17:
-        gender = 1
-    else:
-        gender = 0
-    return result1, result2, result3, gender
-
-
-def y_percussive_criterion(y):
-    y_percussive = round(np.mean(librosa.effects.hpss(y)[1]) * 10 ** 5, 2)
-    if y_percussive > -6:
-        gender = 0
-    else:
-        gender = 1
-    return y_percussive, gender
-
-
-def Xdb_criterion(y):
-    X = librosa.stft(y)
-    Xdb = librosa.amplitude_to_db(abs(X))
-    if np.var(Xdb) > 180:
-        result = 1
-    else:
-        result = 0
-    return np.var(Xdb), result
-
-
-s_sr([i for i in os.listdir() if i[-4:] == ".wav"])
