5c3f6454009bf7ce4ceae1ff1f733a7d1502110f
aminealist <aleshkov.d.o@gmail.com>
2023-04-17 09:24:42 +0700

    Update wav_convert_mp3_remove.py

--- scripts/wav_convert_mp3_remove.py
+++ scripts/wav_convert_mp3_remove.py
@@ -1,5 +1,4 @@
 import os
-from os import path
 from pydub import AudioSegment
 
 
@@ -7,7 +6,7 @@ from pydub import AudioSegment
 # Так же её нужно добавить в "Advanced system settings" в "Environment Variables" в "System Variables" в "Path"
 # В фукцию подается путь аудиофайла в формате mp3, например: "G:\sd-gender-recognizer\file.mp3"
 def wav_convert(input_file):
-    # Проверяем расширение конвертируемого файла
+    # Проверяем расширение конвертируемого файла feeflk
     if input_file[-4:] == ".mp3":
         # Задаем путь финального файла
         output_file = input_file[:-4] + ".wav"
@@ -21,7 +20,18 @@ def wav_convert(input_file):
         sound.export(output_file, format="wav")
 
 
-def all_convert():
+def all_convert(lstdir):
     # Перебор всех файлов из папки
-    for i in [i for i in os.listdir() if i[-4:] == ".mp3" or i[-4:] == "wav"]:
+    for i in [i for i in os.listdir(lstdir) if i[-4:] == ".mp3" or i[-4:] == "wav"]:
         wav_convert(i)
+    mp3_remove(lstdir=lstdir)
+
+
+def mp3_remove(lstdir):
+    lstdir = os.listdir(lstdir)
+    for i in lstdir:
+        if i.endswith(".mp3"):
+            os.remove(i)
+
+
+all_convert(lstdir="")
