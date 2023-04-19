3802b47c831cead275ea4c9cdfd8cff1494f0afa
Sagiman <sasha@sagiman.ru>
2023-04-19 09:00:48 +0700

    analyzer update

--- scripts/database_analiser.py
+++ scripts/database_analiser.py
@@ -3,19 +3,21 @@ from os import listdir
 from audio_func import global_result
 
 
-def data_analyzer(p1, p2, p3):
+def data_analyzer(p1, p2, p3, i1):
     m_err = 0
     f_err = 0
 
-    analyzer("G:\ruls_data\train\audio\9014\11018")
+    analyzer("G:/ruls_data/train/audio/9014/11018/", "f", p1, p2, p3, i1, 1)
 
 
-def analyzer(path, speaker_gender):
-    n = len(self.file_ch_res)
+def analyzer(path, speaker_gender, p1, p2, p3, i1, i2):
+    files = listdir(path=path)
+
+    n = len(files)
     res = ["" for i in range(n)]
 
     for i in range(n):
-        p_res = global_result(self.file_ch_res[i])
+        p_res = global_result(self.file_ch_res[i], p1, p2, p3)
 
         if p_res[0].lower() == self.file_ch_res[i][self.file_ch_res[i].rfind("/") + 1]:
             res[i] = p_res + self.file_ch_res[i] + "\n"
@@ -23,10 +25,6 @@ def analyzer(path, speaker_gender):
             res[i] = p_res + "(err) - " + self.file_ch_res[i] + "\n"
 
         print(str(i + 1) + "/" + str(n))
-
-    with open(self.path_ch_res + "/results.txt", mode="w", encoding="utf-8") as file:
-        for i in range(n):
-            file.write(res[i])
 
 
 mfccs_crit = 11
@@ -39,7 +37,7 @@ for i in range(11):
     per_cri += 0.5
     Xdb_crit += 2
 
-    data_analyzer(mfccs_crit, per_cri, Xdb_crit)
+    data_analyzer(mfccs_crit, per_cri, Xdb_crit, i)
 
 
 #with open("results.txt", mode="w", encoding="utf-8") as file:
