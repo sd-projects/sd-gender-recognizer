e41c47bb8a7bb0abe94386fbda1f8a5c7577d392
Sagiman <sasha@sagiman.ru>
2023-04-20 09:17:56 +0700

    database_analyzer and results of analyze

--- scripts/database_analyzer.py
+++ scripts/database_analyzer.py
@@ -1,101 +1,70 @@
 from os import listdir
 
-from audio_func import global_result
-
-import time
+from audio_func import half_result
 
 
-start = time.time()
+def data_analyzer(data):
+
+    for gender in range(len(data)):
+        k1 = []
+        k2 = []
+        k3 = []
+
+        for path_num in range(len(data[gender])):
+            path = data[gender][path_num]
+            files = listdir(path=path)
+
+            for cur_file in range(len(files)):
+                r1, r2, r3 = half_result(path + files[cur_file])
+
+                k1.append(str(r1) + "\n")
+                k2.append(str(r2) + "\n")
+                k3.append(str(r3) + "\n")
+
+                print(f"Progress: ({gender + 1}/2)({path_num + 1}/{len(data[gender])})({cur_file + 1}/{len(files)})")
+
+        with open(f"results_{gender + 1}_1.txt", mode="w", encoding="utf-8") as rec_file:
+            rec_file.writelines(k1)
+
+        with open(f"results_{gender + 1}_2.txt", mode="w", encoding="utf-8") as rec_file:
+            rec_file.writelines(k2)
+
+        with open(f"results_{gender + 1}_3.txt", mode="w", encoding="utf-8") as rec_file:
+            rec_file.writelines(k3)
 
 
-def data_analyzer(p1, p2, p3, i1):
-    m_data = ["D:/ruls_data/train/audio/8086/7771/", "D:/ruls_data/train/audio/8169/12256/",
-              "D:/ruls_data/test/audio/2826/2145/", "D:/ruls_data/test/audio/4471/2145/",
-              "D:/ruls_data/test/audio/4372/2145/"]
-    m_errs = [0 for _ in range(4)]
-
-    f_data = ["D:/ruls_data/train/audio/9014/11018/", "D:/ruls_data/train/audio/9014/8641/",
-              "D:/ruls_data/test/audio/2671/2145/", "D:/ruls_data/test/audio/3056/2145/",
-              "D:/ruls_data/dev/audio/5397/2145/"]
-    f_errs = [0 for _ in range(4)]
-
-    for i in range(5):
-        temp_errs = analyzer(m_data[i], "m", p1, p2, p3, i1, i)
-
-        for j in range(4):
-            m_errs[j] += temp_errs[j]
-
-    for i in range(5):
-        temp_errs = analyzer(f_data[i], "f", p1, p2, p3, i1, i + 5)
-
-        for j in range(4):
-            f_errs[j] += temp_errs[j]
-
-    res = [f"Coefficients: {p1}, {p2}, {p3}", m_errs, f_errs]
-
-    return res
+def analyzer(k1, k2, k3):
 
 
-def analyzer(path, speaker_gender, p1, p2, p3, i1, i2):
-    files = listdir(path=path)
+analyze_files = True
 
-    n = len(files)
-    errs = [0 for _ in range(4)]
+data = [["D:/ruls_data/train/audio/8086/7771/", "D:/ruls_data/train/audio/8169/12256/",
+         "D:/ruls_data/test/audio/2826/2145/", "D:/ruls_data/test/audio/4471/2145/",
+         "D:/ruls_data/test/audio/4372/2145/", "D:/ruls_data/my/male/"],
+        ["D:/ruls_data/train/audio/9014/11018/", "D:/ruls_data/train/audio/9014/8641/",
+         "D:/ruls_data/test/audio/2671/2145/", "D:/ruls_data/test/audio/3056/2145/",
+         "D:/ruls_data/dev/audio/5397/2145/", "D:/ruls_data/test/audio/5548/2145/",
+         "D:/ruls_data/test/audio/4091/2145/", "D:/ruls_data/my/female/"]]
 
-    for i in range(n):
-        r1, r2, r3 = global_result(path + str(files[i]), p1, p2, p3)
-
-        print(f"Progress ({i1 + 1}/11)({i2 + 1}/10)({i + 1}/{n})")
-
-        if speaker_gender == "m":
-            if r1 == -1:
-                errs[0] += 1
-            if r2 == -1:
-                errs[1] += 1
-            if r3 == -1:
-                errs[2] += 1
-            if r1 + r2 + r3 < 0:
-                errs[3] += 1
-        elif speaker_gender == "f":
-            if r1 == 1:
-                errs[0] += 1
-            if r2 == 1:
-                errs[1] += 1
-            if r3 == 1:
-                errs[2] += 1
-            if r1 + r2 + r3 > 0:
-                errs[3] += 1
-
-    return errs
+if analyze_files:
+    data_analyzer(data)
 
 
-mfccs_crit = 11
-per_cri = -7.5
-Xdb_crit = 168
+analyze_results = False
 
-fin_res = [[] for _ in range(11)]
+mfccs_crit = 6
+step_mfccs_crit = 1
 
-for i in range(11):
-    mfccs_crit += 1
-    per_cri += 0.5
-    Xdb_crit += 2
+per_cri = -11
+step_per_cri = 0.5
 
-    fin_res[i] = data_analyzer(mfccs_crit, per_cri, Xdb_crit, i)
+xdb_crit = 158
+step_xdb_crit = 2
 
-with open("results.txt", mode="w", encoding="utf-8") as file:
-    for i in range(11):
-        for j in range(3):
-            if type(fin_res[i][j]) == type([]):
-                for k in range(4):
-                    file.write(str(fin_res[i][j][k]) + " ")
-                file.write("\n")
-            else:
-                file.write(str(fin_res[i][j]) + "\n")
-        file.write("\n")
+if analyze_results:
+    for i in range(21):
+        mfccs_crit += step_mfccs_crit
+        per_cri += step_per_cri
+        xdb_crit += step_xdb_crit
 
-print("Результаты анализа сохранены")
-
-
-end = time.time() - start
-
-print(end)
+        analyzer(mfccs_crit, per_cri, xdb_crit)
